95ab97fa1c3cdf024c859e7bda2b4ddf138ab311
sahdeshi <59895777+sahdeshi@users.noreply.github.com>
2020-02-17 16:19:57 -0500

    example_for_pull

--- src/practice/main.java
+++ src/practice/main.java
@@ -4,6 +4,7 @@ package practice;
 public class main {
 
     public static void main(String[] args) {
+        System.out.println("what is life");
         System.out.println("yo yo banta rapper");
         System.out.println(Math.random());
     }
